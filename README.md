76e3fd2768426b50efb43d908aaec98d9dc8d6f1
deepanshu sharma <102576705+deepanshuxharry@users.noreply.github.com>
2024-07-19 02:42:19 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Visual Sort <img src="/src/icon.svg" width="48px" height="48px">
+
+-   This is a sorting visualizer application built using React.js
+-   The algorithms that can be visualized currently are **bubble sort**, **insertion sort**, **merge sort** and **quick sort**.
+-   The application is responsive in nature and has a dark theme by default, but it has a theme switch which can be used to toggle the light theme.
---
+++ public/worker.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-restricted-globals */
+var CACHE_NAME = "version3";
+var urlsToCache = ["/"];
+
+// Install a service worker
+self.addEventListener("install", (event) => {
+    // Perform install steps
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function (cache) {
+                console.log("Opened cache");
+                return cache.addAll(urlsToCache);
+            })
+            .then(() => self.skipWaiting())
+    );
+});
+
+// Cache and return requests
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            // Cache hit - return response
+            if (response) {
+                return response;
+            }
+            return fetch(event.request);
+        })
+    );
+});
+
+// Update a service worker
+self.addEventListener("activate", (event) => {
+    var cacheWhitelist = ["version3"];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
